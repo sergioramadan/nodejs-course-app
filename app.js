5ea162e186b50a8380f03a8b395d969c90cc7f21
Sergio Ramadan <sramadan@secureauth.com>
2019-05-27 16:55:30 -0300

    Implementación de nodemon para refrezcar el serve al realizar cambios

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ let app = http.createServer(routes);
 
 // Start the server on port 3000
 app.listen(3000);
-console.log('Node server running on port 3000');
+console.log('Node server running and listening on port 3000');
