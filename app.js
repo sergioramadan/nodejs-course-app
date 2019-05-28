397d153b095161c82c836c4efe7d44a3a6f41866
Sergio Ramadan <sramadan@secureauth.com>
2019-05-28 11:31:35 -0300

    body-parser implementation and update router structure

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
-// Create an instance of the http server to handle HTTP requests
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', (req, res, next) => {
+    console.log('This allways runs!');
+    next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+    console.log('In the middleware!');
+    res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
     console.log('In another middleware!');
     res.send('<h1>Hello from express!</h1>');
 });
