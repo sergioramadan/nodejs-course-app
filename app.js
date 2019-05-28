83a5785a9bddf133da7335e1085735b177417906
Sergio Ramadan <sramadan@secureauth.com>
2019-05-28 13:04:00 -0300

    Upddate path to templates and create 404 view

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -9,10 +10,10 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/admin', adminRoutes);
-app.use('/shop', shopRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).end('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 // Start the server on port 3000
--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,14 @@
+const path = require('path');
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 // GET /admin/add-product
 router.get('/add-product', (req, res, next) => {
     console.log('In the middleware!');
-    res.send(
-        '<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>'
-    );
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // POST /admin/add-product
