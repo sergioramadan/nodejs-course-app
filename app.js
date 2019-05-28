b268b4656a05d34ae56ae2fb305534746bd52125
Sergio Ramadan <sramadan@secureauth.com>
2019-05-28 11:12:06 -0300

    Integración inicial de Express

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
 
 // Create an instance of the http server to handle HTTP requests
-let app = http.createServer(routes);
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from express!</h1>');
+});
 
 // Start the server on port 3000
 app.listen(3000);
