78a98bfc14eb897858e0ac053196a6049862bff9
Sergio Ramadan <sramadan@secureauth.com>
2019-05-28 12:28:34 -0300

    Routes refactor and default page

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', (req, res, next) => {
-    console.log('This allways runs!');
-    next();
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.get('/add-product', (req, res, next) => {
-    console.log('In the middleware!');
-    res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from express!</h1>');
+app.use((req, res, next) => {
+    res.send('<h1>Page not found</h1>');
 });
 
 // Start the server on port 3000
