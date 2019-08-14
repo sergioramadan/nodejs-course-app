a35a6e1f9e719f57ab4236bad41d4960c27d9301
Sergio Ramadan <sergio86r@gmail.com>
2019-08-14 16:58:06 -0300

    EJS implementation and configuration fixes

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.set('view engine', 'pug'); // Setting Pug as template tool
-//app.set('view', 'views');
+app.set('view engine', 'ejs'); // Setting Ejs as template tool
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,7 +17,6 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
     res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
--- routes/shop.js
+++ routes/shop.js
@@ -7,9 +7,15 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
     const products = adminData.products || []; // Products info
-    res.render('shop', { pageTitle: 'Shop', path: '/', prods: products }); // Pug View to render and parameters
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    });
 });
 
 module.exports = router;
