8703cfe6662d10019213f3feb01f8bbd6dd57eff
Sergio Ramadan <sergio86r@gmail.com>
2019-08-15 17:26:00 -0300

    Update admin.js service handler

--- routes/admin.js
+++ routes/admin.js
@@ -7,13 +7,18 @@ const router = express.Router();
 
 // GET /admin/add-product
 router.get('/add-product', (req, res, next) => {
-    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+      });
 });
 
 // POST /admin/add-product
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    products.push({ title: req.body.title });
     res.redirect('/');
 });
 
